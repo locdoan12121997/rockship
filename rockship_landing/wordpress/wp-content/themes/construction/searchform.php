6d70bb3cc42f78e925f54628ec000d538dc67445
locdoan12121997 <cs2015_loc.dtt@student.vgu.edu.vn>
2018-06-15 16:28:29 +0700

    delete unused theme

--- rockship_landing/wordpress/wp-content/themes/construction/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div>
-		<input value="" name="s" id="s" type="text" placeholder="<?php esc_html_e( 'Search', 'construction' ); ?>">
-		<button type="submit"></button>
-	</div>
-</form> 
--- rockship_landing/wordpress/wp-content/themes/construction/templates-part/header/top-bar-menu.php
+++
@@ -1,8 +0,0 @@
-<div class="top-links">
-	<?php
-	// Pages Menu
-	if( has_nav_menu( 'top-menu' ) ) :
-	    wp_nav_menu( array ( 'theme_location' => 'top-menu', 'container' => 'div', 'container_class' => 'navi top-nav', 'menu_class' => '', 'menu_id' => 'main-nav', 'depth' => 1 ) );
-	endif;
-	?>
-</div>
